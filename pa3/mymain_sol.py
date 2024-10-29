27cdf21d7ca97fa58639b366c63f87318f9b6629
mayukh <dasmayukh2002@gmail.com>
2024-10-28 17:29:56 -0700

    completed PA3, beginning PA4

--- pa3/mymain_sol.py
+++ pa3/mymain_sol.py
@@ -2,14 +2,35 @@ import gymnasium as gym
 from mypa3_mf_control import *
 
 if __name__ == '__main__':
-    env = gym.make('CliffWalking-v0')
-    # ags = {'SARSA' : SARSAAgent(env), 'Q-Learning' : QLAgent(env), 'MC' : MCCAgent(env)}
-    ags = {'SARSA' : SARSAAgent(env)}
 
-    # Run each agent once. 
-    for name, agent in ags.items():
-        agent.learn()
-        epi, done = agent.best_run()
-        print(f'{name}: Best Episode: {epi}, Return: {agent.calc_return(epi, done)}')
+    env_names = ['CliffWalking-v0', 'FrozenLake-v1', "Taxi-v3"]
 
+    for name in env_names:
+        print(name)
+        env = gym.make(name)
 
+        # MC
+        name = "MC"
+        for _ in range(5):
+            agent = MCCAgent(env)
+            agent.learn()
+            epi, done = agent.best_run()
+            print(f'{name}: Return: {agent.calc_return(epi, done)}, Best Episode: {epi}')
+        print()
+
+        # SARSA
+        name = "SARSA"
+        for _ in range(5):
+            agent = SARSAAgent(env)
+            agent.learn()
+            epi, done = agent.best_run()
+            print(f'{name}: Return: {agent.calc_return(epi, done)}, Best Episode: {epi}')
+        print()
+
+        # Q-Learning
+        name = "Q-Learning"
+        for _ in range(5):
+            agent = QLAgent(env)
+            agent.learn()
+            epi, done = agent.best_run()
+            print(f'{name}: Return: {agent.calc_return(epi, done)}, Best Episode: {epi}')
--- pa3/mypa3_mf_control.py
+++ pa3/mypa3_mf_control.py
@@ -1,10 +1,11 @@
-from typing import Any
+from typing import Any, List
 import random
 import gymnasium as gym
 import numpy as np
+from collections import Counter
 
 
-def argmax_action(d: dict[Any,float]) -> Any:
+def argmax_action(d: dict[Any, float]) -> Any:
     """return a key of the maximum value in a given dictionary 
 
     Args:
@@ -15,8 +16,10 @@ def argmax_action(d: dict[Any,float]) -> Any:
     """
     return max(d, key=d.get)
 
+
 class ValueRLAgent():
-    def __init__(self, env: gym.Env, gamma : float = 0.98, eps: float = 0.2, alpha: float = 0.02, total_epi: int = 5_000) -> None:
+    def __init__(self, env: gym.Env, gamma: float = 0.98, eps: float = 0.2, alpha: float = 0.02,
+                 total_epi: int = 5_000) -> None:
         """initialize agent parameters
         This class will be a parent class and not be called directly.
 
@@ -33,8 +36,8 @@ class ValueRLAgent():
         self.eps = eps
         self.alpha = alpha
         self.total_epi = total_epi
-    
-    def init_qtable(self, n_states: int, n_actions: int, init_val: float = 0.0) -> dict[int,dict[int,float]]:
+
+    def init_qtable(self, n_states: int, n_actions: int, init_val: float = 0.0) -> dict[int, dict[int, float]]:
         """initialize the q table (dictionary indexed by s, a) with a given init_value
 
         Args:
@@ -67,7 +70,6 @@ class ValueRLAgent():
         else:
             return argmax_action(self.q[state])
 
-
     def choose_action(self, ss: int) -> int:
         """a helper function to specify a exploration policy
         If you want to use the eps_greedy, call the eps_greedy function in this function and return the action.
@@ -80,8 +82,7 @@ class ValueRLAgent():
         """
         return self.eps_greedy(ss)
 
-
-    def best_run(self, max_steps: int = 100) -> tuple[list[tuple[int,int,float]], bool]:
+    def best_run(self, max_steps: int = 100) -> tuple[list[tuple[int, int, float]], bool]:
         """After the learning, an optimal episode (based on the latest self.q) needs to
         be generated for evaluation. From the initial state, always take the greedily best
         action until it reaches a goal.
@@ -99,8 +100,8 @@ class ValueRLAgent():
         """
         episode = []
         reached_terminal_state = False
-        current_state, info  = self.env.reset()
-        
+        current_state, info = self.env.reset()
+
         for step in range(max_steps):
             if reached_terminal_state:
                 break
@@ -115,7 +116,7 @@ class ValueRLAgent():
 
         return episode, reached_terminal_state
 
-    def calc_return(self, episode: list[tuple[int,int,float]], done=False) -> float:
+    def calc_return(self, episode: list[tuple[int, int, float]], done=False) -> float:
         """Given an episode, calculate the return value. An episode is in this format: [(s1,a1,r1), (s2,a2,r2), ...].
 
         Args:
@@ -128,11 +129,41 @@ class ValueRLAgent():
         episode_reward_by_step = []
         for step_num in range(len(episode)):
             step = episode[step_num]
-            episode_reward_by_step.append(episode[step_num][2] * \
-                                          self.gamma ** step_num)
+            episode_reward_by_step.append(step[2] * self.gamma ** step_num)
         return sum(episode_reward_by_step)
 
+
 class MCCAgent(ValueRLAgent):
+
+    def gen_episode(self) -> List:
+        episode = []
+
+        s, info = self.env.reset()  # 36
+
+        # Loop for each step until the end of the episode
+        step_num = 0
+        done = False
+        while not done:
+            if step_num == 500:
+                return episode
+            # Choose A from S using policy derived from Q (greedy)
+            a = self.eps_greedy(s)
+            # Take Action A ==> observe R, S'
+            ss, r, done, truncated, info = self.env.step(a)
+            episode.append((s, a, r))
+            s = ss
+            step_num += 1
+
+        return episode
+
+    def init_rtable(self) -> dict:
+        r = {}
+        for s in range(self.env.observation_space.n):
+            r[s] = {}
+            for a in range(self.env.action_space.n):
+                r[s][a] = []
+        return r
+
     def learn(self) -> None:
         """Monte Carlo Control algorithm
         Update the Q table (self.q) for self.total_epi number of episodes.
@@ -141,10 +172,24 @@ class MCCAgent(ValueRLAgent):
 
         The results should be reflected to its q table.
         """
+        # pi <-- eps-greedy
+        # q-table <-- (already initialized)
+        returns = self.init_rtable()
 
-        max_steps = 500
-        
-        pass
+        for epi in range(self.total_epi):
+            sample_episode = self.gen_episode()
+            state_action_freq_dict = Counter([(step[0], step[1]) for step in sample_episode])
+            G = 0
+
+            for step in reversed(sample_episode):
+                state, action, reward = step
+                G = self.gamma * G + reward
+                state_action_freq_dict[(state, action)] -= 1
+                if state_action_freq_dict[(state, action)] == 0:
+                    returns[state][action].append(G)
+                    self.q[state][action] = np.average(returns[state][action])
+                    del state_action_freq_dict[(state, action)]
+
 
 class SARSAAgent(ValueRLAgent):
     def learn(self) -> None:
@@ -168,41 +213,38 @@ class SARSAAgent(ValueRLAgent):
             done = False
             while not done:
                 # Take Action A ==> observe R, S'
-
-                # next_state, reward, reached_terminal_state, truncated, info, done = self.env.step(greedy_action)
-
-                ss, r, done,truncated, info = self.env.step(a)
-                # print(f's,a,r,ss: {s}, {a}, {r}, {ss}')
-
+                ss, r, done, truncated, info = self.env.step(a)
                 # Choose A from S' using policy derived from Q (greedy)
                 aa = self.eps_greedy(ss)
-                # Q(S,A) <- Q(S,A) + alpha[R + gamma*Q(S',A') - Q(S,A)]
-                # update Q value based on (s,a,r,ss)
-                # (s, a, r, ss)
-                # Q(ss, ??)
-
-                # print("ss:", ss)
-                # print("action map", self.q[ss])
-                # print("aa:", aa)
-                # print("q value for action", self.q[ss][aa])
-
+                # Update Equation
                 bootstrapped_estimate = r + self.gamma * self.q[ss][aa]
                 self.q[s][a] += self.alpha * (bootstrapped_estimate - self.q[s][a])
                 # S <- S', A <- A';
-                s = ss
-                a = aa
+                s, a = ss, aa
 
-class QLAgent(SARSAAgent):  
+
+class QLAgent(SARSAAgent):
     def learn(self):
         """Q-Learning algorithm
         Update the Q table (self.q) for self.total_epi number of episodes.
 
         The results should be reflected to its q table.
-        """   
-        pass
-
-    def choose_action(self, ss: int) -> int:
         """
-        [optional] You may want to override this method.
-        """   
-        pass
+        # Algo Params: alpha and epsilon (already initialized)
+        # Q Table (already initialized)
+        # Loop for each episode
+        for epi in range(self.total_epi):
+            # Initialize S
+            s, info = self.env.reset()
+
+            # Loop for each step of episode:
+            done = False
+            while not done:
+                # Choose A rom S using policy derived from Q (e.g., epsilon-greedy)
+                a = self.eps_greedy(s)
+                # Take action A, observe R, S'
+                ss, r, done, truncated, info = self.env.step(a)
+                # Update Equation
+                self.q[s][a] += self.alpha * (r + self.gamma * max(self.q[ss].values()) - self.q[s][a])
+                # S <-- S'
+                s = ss
